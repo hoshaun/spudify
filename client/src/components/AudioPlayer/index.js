3f4397d0e27c20befd5bdef888d2e6328e9b1668
hoshaun <shaun.ho@mail.utoronto.ca>
2023-05-15 15:56:02 -0400

    refactor

--- client/src/components/AudioPlayer/index.js
+++ client/src/components/AudioPlayer/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 import { useRef, useState } from 'react';
-import { tracks } from './Tracks';
+import beautiful from './SampleData/a_beautiful_day.mp3';
+import world from './SampleData/We_Are_The_World.mp3';
 
 import DisplayTrack from './DisplayTrack';
 import Controls from './Controls';
 import ProgressBar from './ProgressBar';
 
-const AudioPlayer = () => {
+export default function AudioPlayer(props) {
+
+  // temp test tracks REMOVE LATER
+  const tracks = [
+    {
+      title: 'Its a beautiful day',
+      src: beautiful,
+      author: 'Trinix ft Rushawn',
+      //thumbnail: trinix,
+    },
+    {
+      title: 'We Are The World',
+      src: world,
+      author: 'Michael Jackson',
+      //thumbnail: trinix,
+    },
+  ];
+
   const [trackIndex, setTrackIndex] = useState(0);
   const [currentTrack, setCurrentTrack] = useState(tracks[trackIndex]);
   const [timeProgress, setTimeProgress] = useState(0);
@@ -24,19 +42,17 @@ const AudioPlayer = () => {
       <div className="inner">
         <DisplayTrack {...{ currentTrack, audioRef, setDuration, progressBarRef }} />
         <Controls
-  audioRef={audioRef}
-  progressBarRef={progressBarRef}
-  duration={duration}
-  setTimeProgress={setTimeProgress}
-  tracks={tracks} 
-  trackIndex={trackIndex}
-  setTrackIndex={setTrackIndex}
-  setCurrentTrack={setCurrentTrack}
-/>
+          audioRef={audioRef}
+          progressBarRef={progressBarRef}
+          duration={duration}
+          setTimeProgress={setTimeProgress}
+          tracks={tracks} 
+          trackIndex={trackIndex}
+          setTrackIndex={setTrackIndex}
+          setCurrentTrack={setCurrentTrack}
+        />
         <ProgressBar {...{ progressBarRef, audioRef, timeProgress, duration }} />
       </div>
     </div>
   );
 };
-
-export default AudioPlayer;
