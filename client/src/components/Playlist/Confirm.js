382f95a32a5a99eb5265bd686065efb179084a5b
Yonin408 <justin12689@gmail.com>
2023-05-18 16:11:50 -0700

    color/icon change

--- client/src/components/Playlist/Confirm.js
+++ client/src/components/Playlist/Confirm.js
@@ -6,8 +6,10 @@ export default function Confirm(props) {
     <main className="playlist__card playlist__card--confirm">
       <h1 className="text--semi-bold">{props.message}</h1>
       <section className="playlist__actions">
-        <Button danger onClick={props.onCancel} >X</Button>
-        <Button confirm onClick={props.onConfirm} >✔</Button>
+      <i className="fa-regular fa-xmark fa-2x" onClick={props.onCancel}></i>
+          {/* <Button danger onClick={props.onCancel} >X</Button> */}
+          <i className="fa-regular fa-check fa-2x" onClick={props.onConfirm}></i>
+          {/* <Button confirm onClick={props.onConfirm} >✔</Button> */}
       </section>
     </main>
   );
