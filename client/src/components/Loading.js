585cf865fccd82807edbc4974a3853b766cb5a05
hoshaun <shaun.ho@mail.utoronto.ca>
2023-05-20 13:56:30 -0400

    update loading css

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -6,6 +6,7 @@ export default function Loading(props) {
   return (
     <main className="loading">
       <img
+        className="spinner"
         src="images/status.png"
         alt="Loading"
       />
