9f9abcf5075fefab2ddd6a693177156df16ce19d
hoshaun <shaun.ho@mail.utoronto.ca>
2023-05-22 15:02:15 -0400

    remove progress bar color

--- client/src/components/AudioPlayer/Controls.js
+++ client/src/components/AudioPlayer/Controls.js
@@ -44,10 +44,10 @@ export default function Controls({
       const currentTime = audioRef.current.currentTime;
       setTimeProgress(currentTime);
       progressBarRef.current.value = currentTime;
-      progressBarRef.current.style.setProperty(
-        '--range-progress',
-        `${(progressBarRef.current.value / duration) * 100}%`
-      );
+      // progressBarRef.current.style.setProperty(
+      //   '--range-progress',
+      //   `${(progressBarRef.current.value / duration) * 100}%`
+      // );
       playAnimationRef.current = requestAnimationFrame(repeat);
     } else {
       cancelAnimationFrame(playAnimationRef.current);
